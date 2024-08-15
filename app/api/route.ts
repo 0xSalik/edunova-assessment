0db63663bba521a2646f39bc826eee8c6b001133
sal <contact@salikkhan.com>
2024-08-15 11:43:41 -0400

    fix: minor bug fixes

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,7 @@
 /**
- * Hello Edunova team, I used simple json files to populate the app from the api routes.
- * I initially contemplated using a document database but this being an assessment app,
- * I decided against it since I'd need to introduce models and set up a remote connection
- * over the vercel edge network causing the app to slow down considerably.
+ * Hello Edunova team, I  initially used  json files to populate the app from the api routes.
+ * but turns out vercel doesn't allow writing to files over serverless functions so I decided
+ * to go with PocketBase, it's a pretty good database built in Golang. It uses SQLite in the backend.
  */
 
 import { NextResponse } from "next/server";
@@ -11,7 +10,7 @@ export async function GET() {
   return NextResponse.json(
     {
       message:
-        "Hello Edunova team! This is an empty route. Please read the top comment in the /api/route.ts file to understand why i went with plain json files.",
+        "Hello Edunova team! This is an empty route meant for health checks.",
     },
     { status: 201 }
   );
